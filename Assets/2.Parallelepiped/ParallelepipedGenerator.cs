402877d86efe55c10ecc6421b1b7043a3295e876
gaitavr <tbiwka@yandex.ru>
2020-09-14 09:25:19 +0300

    Initial parallelepiped

---
+++ Assets/2.Parallelepiped/ParallelepipedGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class ParallelepipedGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private int _xSize, _ySize, _zSize;
+
+    private Vector3[] _vertices;
+    private Mesh _mesh;
+
+    private void Start()
+    {
+        Generate();
+    }
+    
+    private void Generate()
+    {
+        _mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = _mesh;
+        _mesh.name = "Parallelepiped";
+
+        //Generate logic
+    }
+}
